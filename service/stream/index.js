fcb4a15a5c1314bc085e7c4a5a9bc1ca8b652436
thien <thien-lebk>
2023-12-19 11:49:47 +0700

    update

--- service/stream/index.js
+++ service/stream/index.js
@@ -1,28 +1,6 @@
 import rtsp from "rtsp-ffmpeg";
 import { run, runFaceRecognition } from "../face/faceDetection.js";
-import puppeteer from'puppeteer';
 
-async function bufferToCanvas(imageBuffer) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  // Create a data URL from the image buffer
-  const dataUrl = `data:image/png;base64,${imageBuffer.toString('base64')}`;
-
-  await page.setContent(`<img src="${dataUrl}" id="image">`);
-  const canvas = await page.evaluate(() => {
-    const img = document.getElementById('image');
-    const canvas = document.createElement('canvas');
-    canvas.width = img.width;
-    canvas.height = img.height;
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0);
-    return canvas;
-  });
-
-  await browser.close();
-  return canvas;
-}
 function getCurrentImageStream(uri) {
   let stream = new rtsp.FFMpeg({ input: uri, quality: 100 });
   return new Promise((resolve, reject) => {
